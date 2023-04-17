c20ea7fa06e6dabc9e9be7ad3ff026c61b3aa525
merveille-572 <djuissimerveille572@gmail.com>
2023-04-17 18:20:57 +0100

    creation home page

--- lib/controlllers/authentification.dart
+++ lib/controlllers/authentification.dart
@@ -1,14 +1,18 @@
 import 'dart:convert';
 
 import 'package:feed/constant/constant.dart';
+import 'package:feed/views/home.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 // ignore: depend_on_referenced_packages
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
 class AuthentificationController extends GetxController {
   final isLoading = false.obs;
+  final token = ''.obs;
+  final box = GetStorage();
   Future register({
     required String name,
     required String username,
@@ -32,14 +36,55 @@ class AuthentificationController extends GetxController {
       );
       if (response.statusCode == 201) {
         isLoading.value = false;
-        print(json.decode(response.body));
+       
+        token.value = json.decode(response.body)['token'];
+        box.write('token', token.value);
+        Get.offAll(() => const HomePage());
+        
       } else {
         isLoading.value = false;
         Get.snackbar('Error', json.decode(response.body)['message'],
             snackPosition: SnackPosition.TOP,
             backgroundColor: Colors.red,
-            colorText: Colors.white
-            );
+            colorText: Colors.white);
+
+        token.value = json.decode(response.body)['token'];
+        box.write('token', token.value);
+      }
+    } catch (e) {
+      isLoading.value = false;
+      print(e.toString());
+    }
+  }
+
+  Future login({
+    required String username,
+    required String password,
+  }) async {
+    try {
+      isLoading.value = true;
+      var data = {
+        'username': username,
+        'password': password,
+      };
+      var response = await http.post(
+        Uri.parse(url + 'login'),
+        headers: {
+          'Accept': 'application/json',
+        },
+        body: data,
+      );
+      if (response.statusCode == 200) {
+        isLoading.value = false;
+        token.value = json.decode(response.body)['token'];
+        box.write('token', token.value);
+        Get.offAll(() => const HomePage());
+      } else {
+        isLoading.value = false;
+        Get.snackbar('Error', json.decode(response.body)['message'],
+            snackPosition: SnackPosition.TOP,
+            backgroundColor: Colors.red,
+            colorText: Colors.white);
 
         print(json.decode(response.body));
       }
--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:feed/controlllers/authentification.dart';
 import 'package:feed/views/register.dart';
 import 'package:feed/views/widget/input_widget.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +13,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final AuthentificationController _authentificationController =
+      Get.put(AuthentificationController());
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size.width;
@@ -32,9 +35,9 @@ class _LoginPageState extends State<LoginPage> {
               height: 30,
             ),
             InputWidget(
-              hintText: 'Email',
+              hintText: 'Username',
               obscureText: false,
-              controller: _emailController,
+              controller: _usernameController,
             ),
             const SizedBox(
               height: 20,
@@ -56,12 +59,24 @@ class _LoginPageState extends State<LoginPage> {
                   vertical: 15,
                 ),
               ),
-              onPressed: () {},
-              child: Text(
-                'Login',
-                style: GoogleFonts.poppins(
-                  fontSize: size * 0.040,
-                ),
+              onPressed: () async {
+                await _authentificationController.login(
+                    username: _usernameController.text.trim(),
+                    password: _passwordController.text.trim());
+              },
+              child: Obx( () {
+                  return _authentificationController.isLoading.value ?
+                  const CircularProgressIndicator(
+                    color: Colors.white,
+                  )
+
+                  :Text(
+                    'Login',
+                    style: GoogleFonts.poppins(
+                      fontSize: size * 0.040,
+                    ),
+                  );
+                }
               ),
             ),
             const SizedBox(
